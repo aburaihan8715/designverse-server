c68b7c444098799ba109249715c5e8f5cb7fe1fe
aburahan8721 <aburaihan8721@gmail.com>
2023-06-27 16:38:05 +0600

    socila login and private route implement done

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // middleware
 app.use(cors());
@@ -10,20 +11,38 @@ app.use(express.json());
 // connection string
 
 // database connection function
-const run = async () => {
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.eujox13.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
   try {
-    /*===================================
-    toys api
-    ====================================*/
+    const classesCollection = client.db("fashionVerseDB").collection("classes");
+
+    /*====================
+    classes related apis
+    ======================*/
+    // get all classes data
+    app.get("/classes", async (req, res) => {
+      const result = await classesCollection.find().toArray();
+      res.send(result);
+    });
+
+    console.log("database connected");
   } finally {
     // Ensures that the client will close when you finish/error
-    // await client.close()
+    // await client.close();
   }
-};
-// call the database connection function
-run().catch((err) => {
-  console.log(err.message);
-});
+}
+run().catch(console.dir);
 
 // home route
 app.get("/", async (req, res) => {
