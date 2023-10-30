59ebaba05a2869bbd4137020dca83a0a8304e801
aburahan8721 <aburaihan8721@gmail.com>
2023-10-30 19:09:36 +0600

    all rating dynamic done

--- src/app.js
+++ src/app.js
@@ -96,21 +96,20 @@ app.patch("/users/role/:id", verifyJWT, verifyAdmin, async (req, res) => {
   const id = req.params.id;
   const data = req.body;
   const query = { _id: new ObjectId(id) };
-  const options = { upsert: true };
+
   const updateDoc = {
     $set: {
       role: data.role,
     },
   };
   const userCollection = await getCollection("users");
-  const result = await userCollection.updateOne(query, updateDoc, options);
+  const result = await userCollection.updateOne(query, updateDoc);
   res.send(result);
 });
 
 // get role
 app.get("/users/role/:email", verifyJWT, async (req, res) => {
   const email = req.params.email;
-  // console.log(email);
   if (req.decoded.email !== email) {
     return res.status(403).send({ error: true, message: "forbidden access!" });
   }
@@ -135,7 +134,6 @@ app.put("/users/:email", verifyJWT, async (req, res) => {
   const email = req.params.email;
   const data = req.body;
   const query = { email: email };
-  const options = { upsert: true };
   const updateDoc = {
     $set: {
       name: data.name,
@@ -143,7 +141,7 @@ app.put("/users/:email", verifyJWT, async (req, res) => {
     },
   };
   const userCollection = await getCollection("users");
-  const result = await userCollection.updateOne(query, updateDoc, options);
+  const result = await userCollection.updateOne(query, updateDoc);
   res.send(result);
 });
 
@@ -157,7 +155,6 @@ cart  related apis start
 // create cart
 app.post("/cart", verifyJWT, async (req, res) => {
   const addToCartData = req.body;
-  // console.log(addToCartData);
   const query = { $and: [{ selectedClassId: { $eq: addToCartData.selectedClassId } }, { instructorEmail: { $eq: addToCartData.instructorEmail } }] };
   const cartCollection = await getCollection("cart");
   const isAlreadyAdded = await cartCollection.findOne(query);
@@ -171,7 +168,6 @@ app.post("/cart", verifyJWT, async (req, res) => {
 // get all cart
 app.get("/cart", verifyJWT, async (req, res) => {
   const userEmail = req.query.email;
-  // console.log(userEmail);
   if (!userEmail) {
     res.send([]);
   }
@@ -223,7 +219,7 @@ app.get("/classes", async (req, res) => {
   return res.send(result);
 });
 
-// get class data by id
+// get class by id
 app.get("/classes/:id", async (req, res) => {
   const id = req.params.id;
   const query = { _id: new ObjectId(id) };
@@ -232,11 +228,10 @@ app.get("/classes/:id", async (req, res) => {
   res.send(result);
 });
 
-// update single field class data
-app.patch("/classes/:id", verifyJWT, verifyInstructor, async (req, res) => {
+// patch field by id
+app.patch("/classes/:id", verifyJWT, verifyAdmin, async (req, res) => {
   const id = req.params.id;
   const data = req.body;
-  const options = { upsert: true };
   const query = { _id: new ObjectId(id) };
   const updateDoc = {
     $set: {
@@ -244,16 +239,14 @@ app.patch("/classes/:id", verifyJWT, verifyInstructor, async (req, res) => {
     },
   };
   const classCollection = await getCollection("classes");
-  const result = await classCollection.updateOne(query, updateDoc, options);
+  const result = await classCollection.updateOne(query, updateDoc);
   res.send(result);
 });
 
-// update single field class data
+// patch feedback by id
 app.patch("/classes/feedback/:id", verifyJWT, verifyAdmin, async (req, res) => {
   const id = req.params.id;
   const data = req.body;
-
-  const options = { upsert: true };
   const query = { _id: new ObjectId(id) };
   const updateDoc = {
     $set: {
@@ -261,17 +254,28 @@ app.patch("/classes/feedback/:id", verifyJWT, verifyAdmin, async (req, res) => {
     },
   };
   const classCollection = await getCollection("classes");
-  const result = await classCollection.updateOne(query, updateDoc, options);
+  const result = await classCollection.updateOne(query, updateDoc);
   res.send(result);
 });
 
-// update name and image
-app.patch("/classes", verifyJWT, async (req, res) => {
+// patch review by id
+app.patch("/classes/review/:id", verifyJWT, async (req, res) => {
+  const id = req.params.id;
+  const { newRating } = req.body;
+  const query = { _id: new ObjectId(id) };
+  const updateDoc = {
+    $push: { rating: newRating },
+  };
+  const classCollection = await getCollection("classes");
+  const result = await classCollection.updateOne(query, updateDoc);
+  res.send(result);
+});
+
+// patch name and image
+app.patch("/classes", verifyJWT, verifyInstructor, async (req, res) => {
   const email = req.query.email;
   const data = req.body;
-
   const query = { instructorEmail: email };
-  const options = { upsert: true };
   const updateDoc = {
     $set: {
       instructorName: data.name,
@@ -279,7 +283,7 @@ app.patch("/classes", verifyJWT, async (req, res) => {
     },
   };
   const classCollection = await getCollection("classes");
-  const result = await classCollection.updateMany(query, updateDoc, options);
+  const result = await classCollection.updateMany(query, updateDoc);
   res.send(result);
 });
 
@@ -287,7 +291,6 @@ app.patch("/classes", verifyJWT, async (req, res) => {
 app.put("/classes/:id", verifyJWT, verifyInstructor, async (req, res) => {
   const id = req.params.id;
   const data = req.body;
-  // console.log(data);
   const query = { _id: new ObjectId(id) };
   const options = { upsert: true };
   const updateDoc = {
@@ -318,10 +321,8 @@ payments related apis start
 app.post("/payments", verifyJWT, async (req, res) => {
   const data = req.body;
   const selectedIClassesIds = data.selectedIClassesIds;
-
   const paymentCollection = await getCollection("payments");
   const insertResult = await paymentCollection.insertOne(data);
-
   const query = { selectedClassId: { $in: data.selectedIClassesIds.map((id) => id) } };
 
   // update cart collection
@@ -335,10 +336,13 @@ app.post("/payments", verifyJWT, async (req, res) => {
 
     if (classToUpdate) {
       // Update studentEnrolled and seats fields
-      if (classToUpdate.studentEnrolled !== null && classToUpdate.studentEnrolled !== undefined) {
+      if (!classToUpdate.studentEnrolled) {
+        classToUpdate.studentEnrolled = 1;
+      } else {
         classToUpdate.studentEnrolled += 1;
       }
 
+      // update seats
       if (classToUpdate.seats > 0) {
         classToUpdate.seats -= 1;
       }
@@ -347,13 +351,11 @@ app.post("/payments", verifyJWT, async (req, res) => {
       await classCollection.updateOne({ classId: selectedIClassesId }, { $set: classToUpdate });
     }
   });
-
   await Promise.all(updatePromises);
-
   res.send({ insertResult, deleteResult });
 });
 
-// get payments data by using email
+// get payments data by email
 app.get("/payments", verifyJWT, async (req, res) => {
   const userEmail = req.query.email;
   if (!userEmail) {
@@ -385,7 +387,6 @@ app.post("/create-payment-intent", verifyJWT, async (req, res) => {
     clientSecret: paymentIntent.client_secret,
   });
 });
-
 /*====================
 payments related apis end
 ======================*/
@@ -400,7 +401,7 @@ app.post("/testimonials", verifyJWT, async (req, res) => {
   res.send(result);
 });
 
-app.get("/testimonials", verifyJWT, async (req, res) => {
+app.get("/testimonials", async (req, res) => {
   const testimonialCollection = await getCollection("testimonials");
   const result = await testimonialCollection.find({}).toArray();
   res.send(result);
